ae7dbce9eefd8de5e0b387172b720d18998a152d
Harish-port <harish.sq@gmail.com>
2021-06-02 22:11:28 +0530

    made

--- Vaccines-Available/src/components/Home/Home.jsx
+++ Vaccines-Available/src/components/Home/Home.jsx
@@ -20,6 +20,7 @@ export default function Home() {
       <div className="vaccine-generator">
         <h1>VACCINES AVAILABLE IN BANGALORE</h1>
         {console.log(vaccines)}
+        {console.log(vaccines)}
       </div>
     </div>
   );
