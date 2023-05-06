51af1fea8b8ed938075d0dbd119289862b37eb26
Harish-port <harish.sq@gmail.com>
2023-05-06 06:30:53 +0530

    Fetched the product from the server and mapped it to the UI

--- simple_ecommerce_react_redux/src/Containers/ProductComponent.js
+++ simple_ecommerce_react_redux/src/Containers/ProductComponent.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-export default function ProductComponent() {
-    const products = useSelector((state) => state.allProducts.products[0]);
-    const { id, title } = products;
+const ProductComponent = () => {
+  const products = useSelector((state) => state.allProducts.products);
+  const renderList = products.map((product) => {
+    const { id, title, image, price, category } = product;
     return (
-        <div className='four column wide'>
-            <div className='card'>
-                <div className='image'>
-                    <div className='content'>
-                        <div>{title}</div>
-                    </div>
-                </div>
+      <div className="four wide column" key={id}>
+        <Link to={`/product/${id}`}>
+          <div className="ui link cards">
+            <div className="card">
+              <div className="image">
+                <img src={image} alt={title} />
+              </div>
+              <div className="content">
+                <div className="header">{title}</div>
+                <div className="meta price">$ {price}</div>
+                <div className="meta">{category}</div>
+              </div>
             </div>
-        </div>
-    )
-}
+          </div>
+        </Link>
+      </div>
+    );
+  });
+  return <>{renderList}</>;
+};
+
+export default ProductComponent;
--- simple_ecommerce_react_redux/src/Containers/ProductDetail.js
+++ simple_ecommerce_react_redux/src/Containers/ProductDetail.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 export default function ProductDetail() {
+  const { productId } = useParams();
+  console.log(productId,"productId");
   return (
     <div>ProductDetail</div>
   )
