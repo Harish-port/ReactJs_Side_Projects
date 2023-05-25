fd8419c5e90f01e2b47b47148f191328a394f47a
Harish-port <harish.sq@gmail.com>
2023-05-25 23:58:49 +0530

    New typescript project learning

--- typescript-tutorial/src/App.tsx
+++ typescript-tutorial/src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import InputField from './components/InputField';
+import TodoList from './components/TodoList';
+import { Todo } from './model';
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([])
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todos) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  }
+  console.log(todos, "tpd");
+
   return (
     <div className="App">
-      Hello world
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList setTodos={setTodos} todos={todos}  />
     </div>
   );
 }
