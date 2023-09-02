da054430a82ded6e4efe49bc1608c5d1e5161ca7
Harish S <harish.sq@gmail.com>
2023-09-02 23:59:45 +0530

    new react project with typescript

---
+++ ReactGPTClone/src/utils/mockJsonData.js
@@ -0,0 +1,56 @@
+const data = [{
+    "Today": [
+        {
+            "chatId": 123,
+            "description": "Random Text"
+        },
+        {
+            "chatId": 34,
+            "description": "Random Text"
+        },
+        {
+            "chatId": 243,
+            "description": "Random Text"
+        },
+        {
+            "chatId": 544,
+            "description": "Random Text"
+        }
+    ],
+    "Yesterday": [
+        {
+            "chatId": 43,
+            "description": "Random Text"
+        }
+    ],
+    "July": [
+        {
+            "chatId": 43,
+            "description": "Random Text"
+        },
+        {
+            "chatId": 4235,
+            "description": "Random Text"
+        },
+        {
+            "chatId": 423,
+            "description": "Random Text"
+        },
+        {
+            "chatId": 423,
+            "description": "Random Text"
+        }
+    ],
+    "June": [
+        {
+            "chatId": 4324,
+            "description": "Random Text"
+        },
+        {
+            "chatId": 433,
+            "description": "Random Text"
+        }
+    ]
+}];
+
+export default data;
